91a872992d9ccc1c0bb6c3d76e44c1cd682e825f
megiakhalkatsi <megi.akhalkatsi@gmail.com>
2020-11-29 00:23:46 +0400

    update header and footer for mobile devices / responsive

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// $('.burger, .overlay').click(function(){
+//     $('.burger').toggleClass('clicked');
+//     $('.overlay').toggleClass('show');
+//     $('nav').toggleClass('show');
+//     $('body').toggleClass('overflow');
+//   });
+
+
+  $(".burger-menu").click(function () {
+    $(this).toggleClass("menu-on");
+    $(".web_navigation").toggleClass("active");
+});
